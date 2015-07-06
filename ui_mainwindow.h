6d70824b66b028b49301d4a14223711990cd1eef
sisi16 <wussjill@gmail.com>
2015-07-06 23:35:44 +0800

    some UI works

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -40,23 +40,15 @@ public:
     QAction *actionOpen;
     QAction *actionSet;
     QAction *actionSave;
+    QAction *actionConfirm;
     QWidget *centralWidget;
     QGridLayout *gridLayout_2;
     QHBoxLayout *horizontalLayout_5;
     QLabel *bgLabel_3;
-    QHBoxLayout *horizontalLayout_3;
-    QPushButton *confirmButton;
-    QPushButton *deleteButton;
-    QPushButton *resumeButton;
-    QPushButton *reverseButton;
-    QPushButton *groupButton;
-    QPushButton *ungroupButton;
     QHBoxLayout *horizontalLayout_6;
     QLabel *bgLabel_4;
-    QGridLayout *gridLayout;
-    QSlider *timeLineSlider;
     QFrame *line;
-    QCheckBox *editCheckBox;
+    QFrame *line_2;
     QHBoxLayout *horizontalLayout_2;
     QPushButton *okButton;
     QPushButton *cancelButton;
@@ -64,10 +56,20 @@ public:
     QPushButton *playButton1;
     QPushButton *pauseButton1;
     QPushButton *stopButton1;
-    QVBoxLayout *vLayout_2;
-    QLabel *bgLabel_2;
+    QGridLayout *gridLayout;
+    QSlider *timeLineSlider;
+    QHBoxLayout *horizontalLayout_3;
+    QCheckBox *editCheckBox;
+    QPushButton *confirmButton;
+    QPushButton *deleteButton;
+    QPushButton *resumeButton;
+    QPushButton *reverseButton;
+    QPushButton *groupButton;
+    QPushButton *ungroupButton;
     QVBoxLayout *vLayout_1;
     QLabel *bgLabel_1;
+    QVBoxLayout *vLayout_2;
+    QLabel *bgLabel_2;
     QPushButton *cutButton;
     QMenuBar *menuBar;
     QMenu *menuFile;
@@ -104,6 +106,11 @@ public:
         actionSet->setIcon(icon1);
         actionSave = new QAction(MainWindow);
         actionSave->setObjectName(QStringLiteral("actionSave"));
+        actionConfirm = new QAction(MainWindow);
+        actionConfirm->setObjectName(QStringLiteral("actionConfirm"));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/Images/icons/tick.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        actionConfirm->setIcon(icon2);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         gridLayout_2 = new QGridLayout(centralWidget);
@@ -120,44 +127,7 @@ public:
         horizontalLayout_5->addWidget(bgLabel_3);
 
 
-        gridLayout_2->addLayout(horizontalLayout_5, 13, 1, 1, 14);
-
-        horizontalLayout_3 = new QHBoxLayout();
-        horizontalLayout_3->setSpacing(6);
-        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
-        confirmButton = new QPushButton(centralWidget);
-        confirmButton->setObjectName(QStringLiteral("confirmButton"));
-        confirmButton->setStyleSheet(QStringLiteral(""));
-
-        horizontalLayout_3->addWidget(confirmButton);
-
-        deleteButton = new QPushButton(centralWidget);
-        deleteButton->setObjectName(QStringLiteral("deleteButton"));
-
-        horizontalLayout_3->addWidget(deleteButton);
-
-        resumeButton = new QPushButton(centralWidget);
-        resumeButton->setObjectName(QStringLiteral("resumeButton"));
-
-        horizontalLayout_3->addWidget(resumeButton);
-
-        reverseButton = new QPushButton(centralWidget);
-        reverseButton->setObjectName(QStringLiteral("reverseButton"));
-
-        horizontalLayout_3->addWidget(reverseButton);
-
-        groupButton = new QPushButton(centralWidget);
-        groupButton->setObjectName(QStringLiteral("groupButton"));
-
-        horizontalLayout_3->addWidget(groupButton);
-
-        ungroupButton = new QPushButton(centralWidget);
-        ungroupButton->setObjectName(QStringLiteral("ungroupButton"));
-
-        horizontalLayout_3->addWidget(ungroupButton);
-
-
-        gridLayout_2->addLayout(horizontalLayout_3, 11, 2, 1, 1);
+        gridLayout_2->addLayout(horizontalLayout_5, 12, 1, 1, 13);
 
         horizontalLayout_6 = new QHBoxLayout();
         horizontalLayout_6->setSpacing(6);
@@ -169,32 +139,21 @@ public:
         horizontalLayout_6->addWidget(bgLabel_4);
 
 
-        gridLayout_2->addLayout(horizontalLayout_6, 14, 1, 1, 14);
-
-        gridLayout = new QGridLayout();
-        gridLayout->setSpacing(6);
-        gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        timeLineSlider = new QSlider(centralWidget);
-        timeLineSlider->setObjectName(QStringLiteral("timeLineSlider"));
-        timeLineSlider->setOrientation(Qt::Horizontal);
-
-        gridLayout->addWidget(timeLineSlider, 0, 0, 1, 1);
-
-
-        gridLayout_2->addLayout(gridLayout, 12, 1, 1, 14);
+        gridLayout_2->addLayout(horizontalLayout_6, 13, 1, 1, 13);
 
         line = new QFrame(centralWidget);
         line->setObjectName(QStringLiteral("line"));
         line->setFrameShape(QFrame::HLine);
         line->setFrameShadow(QFrame::Sunken);
 
-        gridLayout_2->addWidget(line, 9, 1, 1, 14);
+        gridLayout_2->addWidget(line, 8, 1, 1, 13);
 
-        editCheckBox = new QCheckBox(centralWidget);
-        editCheckBox->setObjectName(QStringLiteral("editCheckBox"));
-        editCheckBox->setStyleSheet(QStringLiteral("font: 75 12pt \"Times New Roman\";"));
+        line_2 = new QFrame(centralWidget);
+        line_2->setObjectName(QStringLiteral("line_2"));
+        line_2->setFrameShape(QFrame::VLine);
+        line_2->setFrameShadow(QFrame::Sunken);
 
-        gridLayout_2->addWidget(editCheckBox, 11, 1, 1, 1);
+        gridLayout_2->addWidget(line_2, 0, 7, 2, 1);
 
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setSpacing(6);
@@ -206,6 +165,7 @@ public:
         sizePolicy1.setVerticalStretch(0);
         sizePolicy1.setHeightForWidth(okButton->sizePolicy().hasHeightForWidth());
         okButton->setSizePolicy(sizePolicy1);
+        okButton->setStyleSheet(QStringLiteral(""));
 
         horizontalLayout_2->addWidget(okButton);
 
@@ -217,7 +177,7 @@ public:
         horizontalLayout_2->addWidget(cancelButton);
 
 
-        gridLayout_2->addLayout(horizontalLayout_2, 6, 3, 1, 1);
+        gridLayout_2->addLayout(horizontalLayout_2, 5, 8, 1, 1);
 
         horizontalLayout_1 = new QHBoxLayout();
         horizontalLayout_1->setSpacing(6);
@@ -244,19 +204,76 @@ public:
         horizontalLayout_1->addWidget(stopButton1);
 
 
-        gridLayout_2->addLayout(horizontalLayout_1, 6, 1, 1, 1);
+        gridLayout_2->addLayout(horizontalLayout_1, 5, 1, 1, 1);
 
-        vLayout_2 = new QVBoxLayout();
-        vLayout_2->setSpacing(6);
-        vLayout_2->setObjectName(QStringLiteral("vLayout_2"));
-        bgLabel_2 = new QLabel(centralWidget);
-        bgLabel_2->setObjectName(QStringLiteral("bgLabel_2"));
-        bgLabel_2->setStyleSheet(QStringLiteral("background-color: rgb(159, 159, 159);"));
+        gridLayout = new QGridLayout();
+        gridLayout->setSpacing(6);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        timeLineSlider = new QSlider(centralWidget);
+        timeLineSlider->setObjectName(QStringLiteral("timeLineSlider"));
+        timeLineSlider->setOrientation(Qt::Horizontal);
 
-        vLayout_2->addWidget(bgLabel_2);
+        gridLayout->addWidget(timeLineSlider, 1, 0, 1, 1);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        editCheckBox = new QCheckBox(centralWidget);
+        editCheckBox->setObjectName(QStringLiteral("editCheckBox"));
+        sizePolicy1.setHeightForWidth(editCheckBox->sizePolicy().hasHeightForWidth());
+        editCheckBox->setSizePolicy(sizePolicy1);
+        editCheckBox->setStyleSheet(QStringLiteral("font: 75 12pt \"Times New Roman\";"));
+
+        horizontalLayout_3->addWidget(editCheckBox);
+
+        confirmButton = new QPushButton(centralWidget);
+        confirmButton->setObjectName(QStringLiteral("confirmButton"));
+        sizePolicy1.setHeightForWidth(confirmButton->sizePolicy().hasHeightForWidth());
+        confirmButton->setSizePolicy(sizePolicy1);
+        confirmButton->setStyleSheet(QStringLiteral(""));
+
+        horizontalLayout_3->addWidget(confirmButton);
+
+        deleteButton = new QPushButton(centralWidget);
+        deleteButton->setObjectName(QStringLiteral("deleteButton"));
+        sizePolicy1.setHeightForWidth(deleteButton->sizePolicy().hasHeightForWidth());
+        deleteButton->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_3->addWidget(deleteButton);
+
+        resumeButton = new QPushButton(centralWidget);
+        resumeButton->setObjectName(QStringLiteral("resumeButton"));
+        sizePolicy1.setHeightForWidth(resumeButton->sizePolicy().hasHeightForWidth());
+        resumeButton->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_3->addWidget(resumeButton);
+
+        reverseButton = new QPushButton(centralWidget);
+        reverseButton->setObjectName(QStringLiteral("reverseButton"));
+        sizePolicy1.setHeightForWidth(reverseButton->sizePolicy().hasHeightForWidth());
+        reverseButton->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_3->addWidget(reverseButton);
+
+        groupButton = new QPushButton(centralWidget);
+        groupButton->setObjectName(QStringLiteral("groupButton"));
+        sizePolicy1.setHeightForWidth(groupButton->sizePolicy().hasHeightForWidth());
+        groupButton->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_3->addWidget(groupButton);
+
+        ungroupButton = new QPushButton(centralWidget);
+        ungroupButton->setObjectName(QStringLiteral("ungroupButton"));
+        sizePolicy1.setHeightForWidth(ungroupButton->sizePolicy().hasHeightForWidth());
+        ungroupButton->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_3->addWidget(ungroupButton);
 
 
-        gridLayout_2->addLayout(vLayout_2, 0, 3, 3, 12);
+        gridLayout->addLayout(horizontalLayout_3, 0, 0, 1, 1);
+
+
+        gridLayout_2->addLayout(gridLayout, 11, 1, 1, 13);
 
         vLayout_1 = new QVBoxLayout();
         vLayout_1->setSpacing(0);
@@ -270,14 +287,26 @@ public:
         vLayout_1->addWidget(bgLabel_1);
 
 
-        gridLayout_2->addLayout(vLayout_1, 0, 1, 3, 2);
+        gridLayout_2->addLayout(vLayout_1, 0, 1, 2, 6);
+
+        vLayout_2 = new QVBoxLayout();
+        vLayout_2->setSpacing(6);
+        vLayout_2->setObjectName(QStringLiteral("vLayout_2"));
+        bgLabel_2 = new QLabel(centralWidget);
+        bgLabel_2->setObjectName(QStringLiteral("bgLabel_2"));
+        bgLabel_2->setStyleSheet(QStringLiteral("background-color: rgb(159, 159, 159);"));
+
+        vLayout_2->addWidget(bgLabel_2);
+
+
+        gridLayout_2->addLayout(vLayout_2, 0, 8, 2, 6);
 
         cutButton = new QPushButton(centralWidget);
         cutButton->setObjectName(QStringLiteral("cutButton"));
         sizePolicy1.setHeightForWidth(cutButton->sizePolicy().hasHeightForWidth());
         cutButton->setSizePolicy(sizePolicy1);
 
-        gridLayout_2->addWidget(cutButton, 6, 2, 1, 1);
+        gridLayout_2->addWidget(cutButton, 5, 2, 1, 1);
 
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
@@ -305,6 +334,7 @@ public:
         menuFile->addAction(actionSave);
         menuEdit->addAction(actionCandidates);
         menuEdit->addAction(actionTest);
+        mainToolBar->addAction(actionSet);
 
         retranslateUi(MainWindow);
 
@@ -327,22 +357,26 @@ public:
         actionSet->setToolTip(QApplication::translate("MainWindow", "Settings", 0));
 #endif // QT_NO_TOOLTIP
         actionSave->setText(QApplication::translate("MainWindow", "save", 0));
+        actionConfirm->setText(QApplication::translate("MainWindow", "Confirm", 0));
+#ifndef QT_NO_TOOLTIP
+        actionConfirm->setToolTip(QApplication::translate("MainWindow", "Confirm", 0));
+#endif // QT_NO_TOOLTIP
         bgLabel_3->setText(QString());
+        bgLabel_4->setText(QString());
+        okButton->setText(QApplication::translate("MainWindow", "OK", 0));
+        cancelButton->setText(QApplication::translate("MainWindow", "Cancel", 0));
+        playButton1->setText(QApplication::translate("MainWindow", "Play", 0));
+        pauseButton1->setText(QApplication::translate("MainWindow", "Pause", 0));
+        stopButton1->setText(QApplication::translate("MainWindow", "Stop", 0));
+        editCheckBox->setText(QApplication::translate("MainWindow", "Edit Mode", 0));
         confirmButton->setText(QApplication::translate("MainWindow", "Confirm", 0));
         deleteButton->setText(QApplication::translate("MainWindow", "Delete", 0));
         resumeButton->setText(QApplication::translate("MainWindow", "Resume", 0));
         reverseButton->setText(QApplication::translate("MainWindow", "Reverse", 0));
         groupButton->setText(QApplication::translate("MainWindow", "Group", 0));
         ungroupButton->setText(QApplication::translate("MainWindow", "Un-group", 0));
-        bgLabel_4->setText(QString());
-        editCheckBox->setText(QApplication::translate("MainWindow", "Edit Mode", 0));
-        okButton->setText(QApplication::translate("MainWindow", "OK", 0));
-        cancelButton->setText(QApplication::translate("MainWindow", "Cancel", 0));
-        playButton1->setText(QApplication::translate("MainWindow", "Play", 0));
-        pauseButton1->setText(QApplication::translate("MainWindow", "Pause", 0));
-        stopButton1->setText(QApplication::translate("MainWindow", "Stop", 0));
-        bgLabel_2->setText(QString());
         bgLabel_1->setText(QString());
+        bgLabel_2->setText(QString());
         cutButton->setText(QApplication::translate("MainWindow", "Cut", 0));
         menuFile->setTitle(QApplication::translate("MainWindow", "File", 0));
         menuEdit->setTitle(QApplication::translate("MainWindow", "Edit", 0));
